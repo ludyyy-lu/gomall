330bae6f4022fb1f552e1528dcc6ae6982f78799
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-07-02 11:24:52 +0800

    订单建表 | readme更新

--- models/order.go
+++ models/order.go
@@ -1,15 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Order struct {
-	ID         uint         `gorm:"primaryKey"`
-	UserID     uint         `gorm:"not null"`
-	Status     string       `gorm:"type:varchar(20);default:'pending'"`
-	TotalPrice float64      `gorm:"type:decimal(10,2);not null"`
+	ID         uint    `gorm:"primaryKey"`
+	UserID     uint    `gorm:"not null"`
+	Status     string  `gorm:"type:varchar(20);default:'pending'"`
+	TotalPrice float64 `gorm:"type:decimal(10,2);not null"`
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
-	OrderItems []OrderItem `gorm:"foreignKey:OrderID"`
+	DeleteAt   gorm.DeletedAt `gorm:"index"` //开启软删除
+	OrderItems []OrderItem    `gorm:"foreignKey:OrderID"`
 }
 
 type OrderItem struct {
@@ -17,7 +22,10 @@ type OrderItem struct {
 	OrderID    uint    `gorm:"not null"`
 	ProductID  uint    `gorm:"not null"`
 	Quantity   int     `gorm:"not null"`
-	UnitPrice  float64 `gorm:"type:decimal(10,2);not null"`
+	UnitPrice  float64 `gorm:"type:decimal(10,2);not null"` //下单时的价格，防止价格变动导致混乱
 	TotalPrice float64 `gorm:"type:decimal(10,2);not null"`
-	CreatedAt  time.Time
+
+	Product   Product `gorm:"foreignKey:ProductID"`
+	CreatedAt time.Time
+	DeleteAt  gorm.DeletedAt `gorm:"index"` //开启软删除
 }

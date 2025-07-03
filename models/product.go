bd36b1578a1da93371166de3a140fb7fcb6d56e4
ludyyy <14663298+ludyyy@user.noreply.gitee.com>
2025-07-03 11:27:40 +0800

    更新readme中有关乐观锁的解释 | 给product加上version字段

--- models/product.go
+++ models/product.go
@@ -9,9 +9,10 @@ type Product struct {
 	Name        string     `gorm:"type:varchar(100);not null" json:"name"`
 	Description string     `gorm:"type:text" json:"description"`
 	Price       float64    `gorm:"not null" json:"price"`
-	Stock       uint        `gorm:"default:0" json:"stock"`
+	Stock       uint       `gorm:"default:0" json:"stock"`
 	ImageURL    string     `gorm:"type:varchar(255)" json:"image_url"`
 	UserID      uint       `gorm:"not null" json:"user_id"`      // 创建者
 	Categories  []Category `gorm:"many2many:product_categories"` // 多对多
 	OnSale      bool       `gorm:"default:true" json:"on_sale"`
+	Version     int        `gorm:"default:1" json:"version"` // 乐观锁版本号
 }
